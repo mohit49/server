09e6490a8557667bfcc9bfd5fe555ce5a8dd2883
mohit49 <mohitanim@gmail.com>
2021-12-12 11:59:05 -0800

    change to get user details

--- app.js
+++ app.js
@@ -25,44 +25,37 @@ app.get('/', function (req, res) {
 
 app.use("/register" ,(req, res) => {
 
-    const userName = req.body.userName;
+    const username = req.body.userName;
     const password = req.body.password;
     const fullName = req.body.fullName;
     const gender = req.body.gender;
     const email = req.body.email;
     const birthDate = req.body.birthDate;
     db.query("SELECT  email  FROM user WHERE email = '"+ email +"'", function(err1, checkMail, field){
-        if(checkMail.length == 0 ) {
-            db.query("SELECT  userName FROM user WHERE  userName = '"+ userName +"'", function(err2, checkUserName, field){
-               
-                if(checkUserName.length == 0 ) {
+        if(checkMail.length === 0 ) {
+            db.query("SELECT  userName FROM user WHERE  userName = '"+ username +"'", function(err2, checkUserName, field) {
+                if(checkUserName.length === 0 ) {
                     res.status(200);
-                    db.query("INSERT INTO user (userName , Password, gender , fullName , email , birthDate) VALUES(?,?,?,?,?,?);", [userName, password, fullName, gender, email, birthDate] ,(err,result)=>{
+                    db.query("INSERT INTO user (userName , Password, gender , fullName , email , birthDate) VALUES(?,?,?,?,?,?);", [username, password, fullName, gender, email, birthDate] ,(err,result)=>{
+                        console.log("result:"+ result)
                         res.json({
                             message:'User registered Sucessfully',
-                            json: [{'userName': userName, 'fullName': fullName,'gender':gender, 'email':email, 'birthday':birthDate}]
-                           
-    
-                            
+                            json: [{'userName': username, 'password':password, 'fullName': fullName, 'gender': gender, 'email': email, 'birthDate': birthDate }]
                         });
                     })
                 } else {
                     res.json({
                         message:'userAlreadyExist',
                         json: checkUserName,
-                       
-
-                        
-                    }) 
+                    })
                 }
-
             })
         }
         else {
             res.json({
                 message:'emailExist',
                 json: checkMail
-            }) 
+            })
         }
 
     })
